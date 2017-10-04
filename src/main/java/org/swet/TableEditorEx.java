6ccabcfd7665d8eba4127a63d0a6cf249a26a540
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-10-03 23:51:57 -0400

    intermediate revision - changed columns 2 and 3 to combobox types

--- src/main/java/org/swet/TableEditorEx.java
+++ src/main/java/org/swet/TableEditorEx.java
@@ -1,10 +1,19 @@
 package org.swet;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+
 // origin : 
 // http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/TableEditorexample.htm
 // http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableSimpleDemo.htm 
 // http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableEditor.htm
+// http://www.java2s.com/Tutorial/Java/0280__SWT/TableCellEditorComboTextandButton.htm
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.custom.TableEditor;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -17,10 +26,48 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
+import org.openqa.selenium.By;
 
 public class TableEditorEx {
+
+	private static Map<String, String> methodTable = new HashMap<>();
+	static { // these are free-hand
+		methodTable.put("CLICK", "clickButton");
+		methodTable.put("CLICK_BUTTON", "clickButton");
+		methodTable.put("CLICK_CHECKBOX", "clickCheckBox");
+		methodTable.put("CLICK_LINK", "clickLink");
+		methodTable.put("CLICK_RADIO", "clickRadioButton");
+		methodTable.put("CLOSE_BROWSER", "closeBrowser");
+		methodTable.put("CREATE_BROWSER", "openBrowser");
+		methodTable.put("ELEMENT_PRESENT", "elementPresent");
+		methodTable.put("GET_ATTR", "getElementAttribute");
+		methodTable.put("GET_TEXT", "getElementText");
+		methodTable.put("GOTO_URL", "navigateTo");
+		methodTable.put("SELECT_OPTION", "selectDropDown");
+		methodTable.put("SET_TEXT", "enterText");
+		methodTable.put("SEND_KEYS", "enterText");
+		methodTable.put("SWITCH_FRAME", "switchFrame");
+		methodTable.put("VERIFY_ATTR", "verifyAttribute");
+		methodTable.put("VERIFY_TEXT", "verifyText");
+		methodTable.put("CLEAR_TEXT", "clearText");
+		methodTable.put("WAIT", "wait");
+	}
+
+	private static Map<String, Method> locatorTable = new HashMap<>();
+
 	public static void main(String[] args) {
+
+		try {
+			locatorTable.put("cssSelector",
+					By.class.getMethod("cssSelector", String.class));
+			locatorTable.put("xpath", By.class.getMethod("xpath", String.class));
+			locatorTable.put("id", By.class.getMethod("id", String.class));
+			locatorTable.put("name", By.class.getMethod("name", String.class));
+		} catch (NoSuchMethodException e) {
+		}
+
 		Display display = new Display();
+
 		Shell shell = new Shell(display);
 		shell.setLayout(new FillLayout());
 		//
@@ -41,9 +88,37 @@ public class TableEditorEx {
 			column.setWidth(100);
 		}
 		for (int i = 0; i < 3; i++) {
-			TableItem item = new TableItem(table, SWT.NONE);
+			new TableItem(table, SWT.NONE);
+		}
+		TableItem[] items = table.getItems();
+
+		for (int i = 0; i < 3; i++) {
+			TableItem item = items[i];
 			item.setText(new String[] { String.format("Element %d name", i),
-					String.format("Action %d", i), String.format("Selector type of element %d", i), String.format("Selector %d", i) });
+					String.format("Action %d", i),
+					String.format("Selector type of element %d", i),
+					String.format("Selector %d", i) });
+
+			TableEditor editor1 = new TableEditor(table);
+			CCombo combo1 = new CCombo(table, SWT.NONE);
+			combo1.setText("CHOOSE");
+			for (String keyword : methodTable.values()) {
+				combo1.add(keyword);
+				System.err.println(keyword);
+			}
+			editor1.grabHorizontal = true;
+			editor1.setEditor(combo1, items[i], 1);
+
+			TableEditor editor2 = new TableEditor(table);
+			CCombo combo2 = new CCombo(table, SWT.NONE);
+			combo2.setText("CHOOSE");
+			for (String locator : locatorTable.keySet()) {
+				combo2.add(locator);
+				System.err.println(locator);
+			}
+			editor2.grabHorizontal = true;
+			editor2.setEditor(combo2, items[i], 2);
+
 		}
 
 		for (int titleItem = 0; titleItem < titles.length; titleItem++) {
