ee1c1396c70fa675cc6a55732474a69491f599db
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-10-05 00:06:16 -0400

    intermediate revision - loading  yaml serialized queue into table

--- src/main/java/org/swet/TableEditorEx.java
+++ src/main/java/org/swet/TableEditorEx.java
@@ -2,15 +2,11 @@ package org.swet;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
-
-// origin : 
-// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/TableEditorexample.htm
-// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableSimpleDemo.htm 
-// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableEditor.htm
-// http://www.java2s.com/Tutorial/Java/0280__SWT/TableCellEditorComboTextandButton.htm
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
@@ -26,7 +22,18 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
+
 import org.openqa.selenium.By;
+
+import org.swet.YamlHelper;
+
+// origin : 
+// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/TableEditorexample.htm
+// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableSimpleDemo.htm 
+// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/SWTTableEditor.htm
+// http://www.java2s.com/Tutorial/Java/0280__SWT/TableCellEditorComboTextandButton.htm
+// http://www.java2s.com/Code/Java/SWT-JFace-Eclipse/DemonstratesTableEditor.htm
+// http://www.java2s.com/Tutorial/Java/0280__SWT/UsingTableEditor.htm
 
 public class TableEditorEx {
 
@@ -54,9 +61,35 @@ public class TableEditorEx {
 	}
 
 	private static Map<String, Method> locatorTable = new HashMap<>();
+	private static Configuration testCase = null;
+	private static HashMap<String, HashMap<String, String>> testData = new HashMap<>();
+	private static String yamlFilePath = null;
 
 	public static void main(String[] args) {
 
+		yamlFilePath = (args.length == 0)
+				? String.format("%s/%s", System.getProperty("user.dir"), "sample.yaml")
+				: args[0];
+
+		if (yamlFilePath != null) {
+			System.err.println("Loading " + yamlFilePath);
+			testCase = YamlHelper.loadConfiguration(yamlFilePath);
+			testData = testCase.getElements();
+			YamlHelper.printConfiguration(testCase);
+		}
+
+		Map<String, Integer> elementSteps = testData.keySet().stream()
+				.collect(Collectors.toMap(o -> o,
+						o -> Integer.parseInt(testData.get(o).get("ElementStepNumber"))));
+		LinkedHashMap<String, Integer> sortedElementSteps = sortByValue(
+				elementSteps);
+		for (String stepId : sortedElementSteps.keySet()) {
+			HashMap<String, String> elementData = testData.get(stepId);
+			// Append row in the TableEditor
+			String commandId = elementData.get("CommandId");
+			System.out.println(String.format("Loading step %s %s", commandId,
+					elementData.get("ElementCodeName")));
+		}
 		try {
 			locatorTable.put("cssSelector",
 					By.class.getMethod("cssSelector", String.class));
@@ -104,8 +137,9 @@ public class TableEditorEx {
 			combo1.setText("CHOOSE");
 			for (String keyword : methodTable.values()) {
 				combo1.add(keyword);
-				System.err.println(keyword);
+				// System.err.println(keyword);
 			}
+			combo1.select(2);
 			editor1.grabHorizontal = true;
 			editor1.setEditor(combo1, items[i], 1);
 
@@ -114,8 +148,9 @@ public class TableEditorEx {
 			combo2.setText("CHOOSE");
 			for (String locator : locatorTable.keySet()) {
 				combo2.add(locator);
-				System.err.println(locator);
+				// System.err.println(locator);
 			}
+			combo2.select(2);
 			editor2.grabHorizontal = true;
 			editor2.setEditor(combo2, items[i], 2);
 
@@ -188,4 +223,55 @@ public class TableEditorEx {
 		}
 		display.dispose();
 	}
+
+	private static void appendRowToTable(Table table, TableItem item,
+			HashMap<String, String> elementData) {
+
+		return;
+
+	}
+
+	private static void appendBlankRowToTable(Table table, TableItem item,
+			int index) {
+
+		item.setText(new String[] { String.format("Element %d name", index),
+				String.format("Action %d", index),
+				String.format("Selector type of element %d", index),
+				String.format("Selector %d", index) });
+
+		TableEditor editor1 = new TableEditor(table);
+		CCombo combo1 = new CCombo(table, SWT.NONE);
+		combo1.setText("CHOOSE");
+		for (String keyword : methodTable.values()) {
+			combo1.add(keyword);
+			// System.err.println(keyword);
+		}
+		combo1.select(2);
+		editor1.grabHorizontal = true;
+		editor1.setEditor(combo1, item, 1);
+
+		TableEditor editor2 = new TableEditor(table);
+		CCombo combo2 = new CCombo(table, SWT.NONE);
+		combo2.setText("CHOOSE");
+		for (String locator : locatorTable.keySet()) {
+			combo2.add(locator);
+			// System.err.println(locator);
+		}
+		combo2.select(2);
+		editor2.grabHorizontal = true;
+		editor2.setEditor(combo2, item, 2);
+		return;
+
+	}
+
+	// TODO: move to Utils.java
+	// sorting example from
+	// http://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values-java
+	public static <K, V extends Comparable<? super V>> LinkedHashMap<K, V> sortByValue(
+			Map<K, V> map) {
+		return map.entrySet().stream().sorted(Map.Entry.comparingByValue())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+						(e1, e2) -> e1, LinkedHashMap::new));
+	}
+
 }
