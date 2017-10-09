8c3c883066fe7253235142f436339d267a20f94a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2017-10-08 20:46:45 -0400

    intermediate revision - passing parameters to java application

--- run.cmd
+++ run.cmd
@@ -1,27 +1,21 @@
 @echo OFF
-
+if "%TOOLS_DIR%"=="" set TOOLS_DIR='c:\java'
 if "%JAVA_VERSION%"=="" set JAVA_VERSION=1.8.0_101
-if "%JAVA_HOME%"=="" set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
+if "%JAVA_HOME%"=="" set JAVA_HOME=%TOOLS_DIR%\jdk%JAVA_VERSION%
 set JAVA_OPTS=-Xms256m -Xmx512m
-
 if "%MAVEN_VERSION%"=="" set MAVEN_VERSION=3.3.9
-if "%M2_HOME%"=="" set M2_HOME=c:\java\apache-maven-%MAVEN_VERSION%
+if "%M2_HOME%"=="" set M2_HOME=%TOOLS_DIR%\apache-maven-%MAVEN_VERSION%
 if "%M2%"=="" set M2=%M2_HOME%\bin
 set MAVEN_OPTS=-Xms256m -Xmx512m
-
 PATH=%JAVA_HOME%\bin;%M2%;%PATH%
-
 set TARGET=%CD%\target
-
 set PACKAGE_NAME=swet
 set PACKAGE_VERSION=0.0.7-SNAPSHOT
 set MAIN_APP_PACKAGE=org.swet
 set MAIN_APP_CLASS=%1
+if NOT "%MAIN_APP_CLASS%"=="" shift
 if "%MAIN_APP_CLASS%"=="" set MAIN_APP_CLASS=SimpleToolBarEx
-
 call mvn -Dmaven.test.skip=true package install
-
 java -cp %TARGET%\%PACKAGE_NAME%-%PACKAGE_VERSION%.jar;%TARGET%\lib\* ^
-%MAIN_APP_PACKAGE%.%MAIN_APP_CLASS%
-
+%MAIN_APP_PACKAGE%.%MAIN_APP_CLASS% %*
 goto :EOF
