1178f5b213fd7f77dd882f4375312892d55b896e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-01-12 13:41:37 -0500

    intermediate revision - added broken log4j configuration test example

--- run.cmd
+++ run.cmd
@@ -22,8 +22,14 @@ REM compile
 call mvn -Dmaven.test.skip=true package install
 
 REM run
-REM NOTE: shift does not affect the value of %* 
-java -Dlog4j.configuration=file:///%APP_HOME%/src/main/resources/log4j.xml -cp %TARGET%\%PACKAGE_NAME%-%PACKAGE_VERSION%.jar;%TARGET%\lib\* ^
+REM NOTE: shift does not affect the value of %*
+java -cp %TARGET%\%PACKAGE_NAME%-%PACKAGE_VERSION%.jar;%TARGET%\lib\* ^
 %MAIN_APP_PACKAGE%.%MAIN_APP_CLASS% %1 %2 %3 %4 %5 %6 %7 %8 %9
+REM ERROR StatusLogger No log4j2 configuration file found. Using default configuration: logging only errors to the console.
+echo on
+java -Dlog4j.configuration=file:///%APP_HOME%/src/main/resources/log4j.properties ^
+-cp %TARGET%\%PACKAGE_NAME%-%PACKAGE_VERSION%.jar;%TARGET%\lib\* ^
+%MAIN_APP_PACKAGE%.%MAIN_APP_CLASS% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@echo off
 :endlocal
 goto :EOF
--- src/main/java/com/github/sergueik/swet/Log4JTest.java
+++ src/main/java/com/github/sergueik/swet/Log4JTest.java
@@ -2,7 +2,8 @@ package com.github.sergueik.swet;
 
 // The org.apache.log4j.Category  has been deprecated 
 // and should be replaced by the Logger
-import org.apache.log4j.Category;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.util.Formatter;
@@ -14,22 +15,21 @@ import java.io.IOException;
 // based on: https://alvinalexander.com/blog/post/java/sample-how-format-log4j-logging-logfile-output
 public class Log4JTest {
 	// our log4j category reference
-	
+
 	private static StringBuilder loggingSb = new StringBuilder();
 	private static Formatter formatter = new Formatter(loggingSb, Locale.US);
 
-	@SuppressWarnings("deprecation")
-	static final Category log = Category.getInstance(Log4JTest.class);
+	private static final Logger logger = LogManager.getLogger(Log4JTest.class);
 	static final String LOG_PROPERTIES_FILE = "src/main/resources/log4J.properties";
 
 	public static void main(String[] args) {
 		new Log4JTest();
-		log.info("Main done.");
+		logger.info("Main done.");
 	}
 
 	public Log4JTest() {
 		initializeLogger();
-		log.info("Log4JTest constructor done.");
+		logger.info("Log4JTest constructor done.");
 	}
 
 	private void initializeLogger() {
@@ -38,10 +38,9 @@ public class Log4JTest {
 		try {
 			logProperties.load(new FileInputStream(LOG_PROPERTIES_FILE));
 			PropertyConfigurator.configure(logProperties);
-			log.info("Initialize Logger.");
+			logger.info("Initialize Logger.");
 		} catch (IOException e) {
-			throw new RuntimeException(
-					"Fail to load: " + LOG_PROPERTIES_FILE);
+			throw new RuntimeException("Fail to load: " + LOG_PROPERTIES_FILE);
 		}
 	}
 }
