2c54d6447d3a2af465df388c70b4cfaf7796bb21
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-18 00:09:31 -0400

    intermediate  revision - moved Log4j2Test to be test, as it should, modified log4j2.xml to use long format for debugging and error messages

--- src/test/java/com/github/sergueik/swet/Log4j2Test.java
+++ src/test/java/com/github/sergueik/swet/Log4j2Test.java
@@ -1,12 +1,21 @@
 package com.github.sergueik.swet;
 
 // for jvm arguments. Must have "-DAPP_LOG_ROOT=c:/temp"
+// can also assert if the -Dfile.encoding=UTF-8 argument is passed into surefile plugin properly
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Common utilities for separate patterns, files for Logging info, error and debug to console and file logging for Selenium WebDriver Elementor Tool (SWET)
@@ -14,12 +23,13 @@ import org.apache.logging.log4j.Logger;
  */
 
 // based on: https://github.com/hmeclazcke/log4j2-example
-public class Log4j2Ex {
+public class Log4j2Test {
 	// This example must run with -DAPP_LOG_ROOT=c:/temp
 	private static final Logger LOGGER = LogManager
-			.getLogger(Log4j2Ex.class.getName());
+			.getLogger(Log4j2Test.class.getName());
 
-	public static void main(String[] args) {
+	@Test
+	public void sampleLog4Jtest() {
 
 		// Prints the jvm arguments. Must have "-DAPP_LOG_ROOT=c:/temp" --> location
 		// of the logs.-
@@ -30,6 +40,7 @@ public class Log4j2Ex {
 		LOGGER.debug("Debug debugging message");
 		LOGGER.info("Info information message");
 		LOGGER.error("Error Message Logged !!!", new Exception("excepition"));
+
 	}
 
 }
