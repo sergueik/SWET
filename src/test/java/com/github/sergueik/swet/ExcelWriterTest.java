eeacb243eff572bf6294168aef0c9a06c2a07d95
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-09-10 23:14:26 +0200

    intermediate revision - partially rolled back and added cleanup method

--- src/test/java/com/github/sergueik/swet/ExcelWriterTest.java
+++ src/test/java/com/github/sergueik/swet/ExcelWriterTest.java
@@ -3,16 +3,17 @@ package com.github.sergueik.swet;
  * Copyright 2019 Serguei Kouzmine
  */
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
-import java.nio.file.Path;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -20,9 +21,16 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -30,12 +38,23 @@ import org.junit.Test;
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
-// see also the forum where the initially failing implementation was shown
+// see also the forum where few initially failing implementations were shown
 // http://software-testing.ru/forum/index.php?/topic/38373-apache-poi-dobavlenie-novoj-stranitcy-v-suschestvuiuschij-fa/
+
 public class ExcelWriterTest {
 	private static List<String> dummyKeys = new ArrayList<>();
 	private static List<String> dummyValues = new ArrayList<>();
 	private static String fileName = "dummy.xlsx";
+	private static Workbook wb = null;
+
+	@AfterClass
+	public static void cleanup() throws IOException {
+		File file = new File(fileName);
+
+		if (!file.delete()) {
+			throw new IOException("Delete " + file.getAbsolutePath() + "failed ");
+		}
+	}
 
 	@Test
 	public void supportedKeywordsContainsKeywordTableTest()
@@ -53,27 +72,36 @@ public class ExcelWriterTest {
 		dummyValues.clear();
 		dummyKeys.add("key 1");
 		dummyKeys.add("key 2");
-		dummyValues.add("10");
-		dummyValues.add("20");
+		dummyValues.add("50");
+		dummyValues.add("60");
 		addSheet("Sheet 2", dummyKeys, dummyValues, fileName);
+
+		wb = new XSSFWorkbook(new FileInputStream(fileName));
+		assertThat(wb.getNumberOfSheets(), greaterThan(1));
+		wb.close();
+		addSheet("Sheet 1", dummyKeys, dummyValues, fileName);
 		// assertTrue(supportedKeywords.containsAll(keywordTable.keySet()));
 		// assertFalse(keywordTable.keySet().containsAll(supportedKeywords));
+		wb = new XSSFWorkbook(new FileInputStream(fileName));
+		assertThat(wb.getNumberOfSheets(), equalTo(2));
+		wb.close();
 	}
 
 	public static void addSheet(String sheetName, List<String> keys,
 			List<String> values, String fileName)
 			throws IOException, EncryptedDocumentException, InvalidFormatException {
-		Workbook wb = null;
+
 		if (Files.exists(FileSystems.getDefault()
 				.getPath(System.getProperty("user.dir"), fileName),
 				new LinkOption[] { LinkOption.NOFOLLOW_LINKS })) {
 
 			System.out.println("File exists: " + fileName);
-			wb = WorkbookFactory.create(new FileInputStream(fileName));
+			wb = new XSSFWorkbook(new FileInputStream(fileName));
 		} else {
 			wb = new XSSFWorkbook();
 		}
-		Sheet sheet = wb.createSheet(sheetName);
+		Sheet sheet = wb.getSheet(sheetName) != null ? wb.getSheet(sheetName)
+				: wb.createSheet(sheetName);
 
 		Row r0 = sheet.createRow(0);
 		Cell c0 = r0.createCell(0);
@@ -104,7 +132,7 @@ public class ExcelWriterTest {
 		FileOutputStream outputStream = new FileOutputStream(fileName);
 
 		wb.write(outputStream);
+		wb.close();
 		outputStream.close();
 	}
-
 }
